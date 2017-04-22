b5372f7dd1c0cd2c56a65fbc18ddef98d2cef7aa
Pavel Kretov <firegurafiku@gmail.com>
2017-04-22 21:00:54 +0300

    Add sketch code for Block class
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

---
+++ include/rvlm/fdtd/Block.hh
@@ -0,0 +1,65 @@
+#pragma once
+#include "rvlm/core/NonAssignable.hh"
+#include "rvlm/core/Cuboid.hh"
+#include "rvlm/core/SolidArray3d.hh"
+#include "rvlm/fdtd/Common.hh"
+
+namespace rvlm {
+namespace fdtd {
+
+class Domain;
+
+template <typename T = double>
+class Block: public rvlm::core::NonAssignable {
+public:
+
+    explicit Block(IndexType nx, IndexType ny, IndexType nz)
+        : mFieldEx(nx, ny, nz)
+        , mFieldEy(nx, ny, nz)
+        , mFieldEz(nx, ny, nz)
+        , mFieldHx(nx, ny, nz)
+        , mFieldHy(nx, ny, nz)
+        , mFieldHz(nx, ny, nz)
+        {}
+
+    typedef rvlm::core::SolidArray3d<T> ArrayType;
+
+    Domain* getDomain() const { return mDomain; }
+
+    rvlm::core::Cuboid<T> getBounds() const { return mBounds; }
+
+    ArrayType& getFieldEx() const { return mFieldEx; }
+
+/* construction: */
+
+    void setDomain(Domain* value) { mDomain = value; }
+    void setBounds(core::Cuboid<T> const& value) { mBounds = value; }
+
+private:
+
+    Domain* mDomain;
+
+    rvlm::core::Cuboid<T> mBounds;
+
+    ArrayType mFieldEx;
+    ArrayType mFieldEy;
+    ArrayType mFieldEz;
+    ArrayType mFieldHx;
+    ArrayType mFieldHy;
+    ArrayType mFieldHz;
+    // ArrayType mMaterialEpsEx;
+    // ArrayType mMaterialEpsEy;
+    // ArrayType mMaterialEpsEz;
+    // ArrayType mMaterialMuHx;
+    // ArrayType mMaterialMuHy;
+    // ArrayType mMaterialMuHz;
+    // ArrayType mMaterialSigmaEx;
+    // ArrayType mMaterialSigmaEy;
+    // ArrayType mMaterialSigmaEz;
+    // ArrayType mMaterialSigmaHx;
+    // ArrayType mMaterialSigmaHy;
+    // ArrayType mMaterialSigmaHz;
+};
+
+} // namespace fdtd
+} // namespace rvlm
---
+++ include/rvlm/fdtd/Common.hh
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstddef>
+
+namespace rvlm {
+namespace fdtd {
+
+typedef ptrdiff_t IndexType;
+
+}
+}
